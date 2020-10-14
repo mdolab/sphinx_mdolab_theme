4ed089712c309aae7f523db1b9f9e7a7bbd47b64
Neil Wu <neilwu@umich.edu>
2020-10-14 16:05:18 -0400

    added extension and html_theme

--- sphinx_mdolab_theme/config.py
+++ sphinx_mdolab_theme/config.py
@@ -12,6 +12,18 @@ master_doc = "index"
 
 # -- Options for HTML output ---------------------------------------------------
 
+# Add any Sphinx extension module names here, as strings. They can be extensions
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.viewcode",
+]
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "sphinx_mdolab_theme"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
