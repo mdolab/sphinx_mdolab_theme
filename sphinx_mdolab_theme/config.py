44719f2c679c638f59525d64af55569786f92736
Neil Wu <neilwu@umich.edu>
2020-10-16 15:37:10 -0400

    added theme options

--- sphinx_mdolab_theme/config.py
+++ sphinx_mdolab_theme/config.py
@@ -24,6 +24,10 @@ extensions = [
 # a list of builtin themes.
 html_theme = "sphinx_mdolab_theme"
 
+html_theme_options = {
+    "titles_only": True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
