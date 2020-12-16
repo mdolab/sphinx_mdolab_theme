6f574b0475f1e8ffbaffd7ee2f4831e1d7e8d7b2
Neil Wu <neilwu@umich.edu>
2020-12-16 13:43:25 -0500

    wrong name for package

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         "sphinx",
         "sphinx_rtd_theme",
-        "yaml",
+        "pyyaml",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
