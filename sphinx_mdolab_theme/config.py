cee369a0d31edd30191c6f08dace527ff29bdd87
Neil Wu <neilwu@umich.edu>
2021-01-08 15:56:40 -0500

    better way to add css

--- sphinx_mdolab_theme/config.py
+++ sphinx_mdolab_theme/config.py
@@ -42,8 +42,6 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_context = {
-    "css_files": [
-        "_static/theme_overrides.css",  # override wide tables in RTD theme
-    ],
-}
+
+def setup(app):
+    app.add_css_file("theme_overrides.css")
