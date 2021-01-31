2a851dcf609980bf7f7a321d9810d0319623576d
Neil Wu <602725+nwu63@users.noreply.github.com>
2021-01-31 13:08:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "pyyaml",
     ],
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 5 - Production/Stable",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
