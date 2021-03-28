f680bf122b3ed0b4cd078a264f66c9e686ebdcec
Neil Wu <neilwu@umich.edu>
2021-03-28 11:42:08 -0400

    perhaps this will make sphinxcontrib.bibtex happy

--- sphinx_mdolab_theme/config.py
+++ sphinx_mdolab_theme/config.py
@@ -52,6 +52,8 @@ html_static_path = ["_static"]
 # a custom logo for a specific documentation site
 html_logo = os.path.join(os.path.dirname(__file__), "static/MDO_Lab_logo_RTD.png")
 
+# this is required for sphinxcontrib.bibtex
+bibtex_bibfiles = []
 
 def setup(app):
     app.add_css_file("theme_overrides.css")
